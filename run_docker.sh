7f3651cbe5488cfbe1947da8fdd463e0e27d5fc8
MD. AREFUL ISLAM <32135229+arifislam007@users.noreply.github.com>
2023-06-13 18:17:36 +0600

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /webdata
 docker build -t nginx:v1 .
-docker run -itd --name web02 -p 95:80 -v /webdata:/usr/share/nginx/html/ nginx:v1
+docker run -itd --name web02 -p 89:80 -v /webdata:/usr/share/nginx/html/ nginx:v1
 cp ./index.html /webdata/
